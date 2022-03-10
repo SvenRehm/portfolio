6d48b77111d138d1965dbda284dd0a652fcb9658
SvenRehm <rehm21@web.de>
2022-03-10 15:58:46 +0100

    Updates

---
+++ precache-manifest.940878ccaae9a852c847ebb84c84e043.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217d6d4633d3b5c395b6e6d3bc07f93f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dbe03b02af33416f127b",
+    "url": "/portfolio/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "b0a91b301d7a7ac82774",
+    "url": "/portfolio/static/css/main.4a80e4f5.chunk.css"
+  },
+  {
+    "revision": "dbe03b02af33416f127b",
+    "url": "/portfolio/static/js/2.0c0fdd00.chunk.js"
+  },
+  {
+    "revision": "b0a91b301d7a7ac82774",
+    "url": "/portfolio/static/js/main.b2a7fe96.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "aeec28632f4519bdb1c51013fead8ccd",
+    "url": "/portfolio/static/media/close-button.aeec2863.svg"
+  },
+  {
+    "revision": "5725812b6be8a648e0939edfc24d933a",
+    "url": "/portfolio/static/media/project1.5725812b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.609b4b5c233fc9da19bb0b1f236c19d6.js"
+  "/portfolio/precache-manifest.940878ccaae9a852c847ebb84c84e043.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
