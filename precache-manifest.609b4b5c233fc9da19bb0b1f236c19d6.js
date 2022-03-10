7774ca2ddff2ca644cf1f8ace39f09058f308fb6
SvenRehm <rehm21@web.de>
2022-03-10 15:55:35 +0100

    Updates

---
+++ precache-manifest.609b4b5c233fc9da19bb0b1f236c19d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "166e7a1783bcba67f1a088eaf759b3da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe03b02af33416f127b",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "27391b86e045e3b2d9ad",
+    "url": "/static/css/main.4a80e4f5.chunk.css"
+  },
+  {
+    "revision": "dbe03b02af33416f127b",
+    "url": "/static/js/2.0c0fdd00.chunk.js"
+  },
+  {
+    "revision": "27391b86e045e3b2d9ad",
+    "url": "/static/js/main.7eb7f436.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aeec28632f4519bdb1c51013fead8ccd",
+    "url": "/static/media/close-button.aeec2863.svg"
+  },
+  {
+    "revision": "5725812b6be8a648e0939edfc24d933a",
+    "url": "/static/media/project1.5725812b.jpg"
+  }
+]);
