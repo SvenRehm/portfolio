ad660c350df37b64ee7acf27350ce074182b4525
SvenRehm <rehm21@web.de>
2022-07-01 00:29:45 +0200

    update

---
+++ build/precache-manifest.6c6e38ae2cac15aa4adce013495da3a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ef788310f8da750cf5a52b2dfaa31c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "56ab78d2bf28202b37bf",
+    "url": "/portfolio/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "625bdeeb2b16d0a0b5d9",
+    "url": "/portfolio/static/css/main.2dc5f903.chunk.css"
+  },
+  {
+    "revision": "56ab78d2bf28202b37bf",
+    "url": "/portfolio/static/js/2.988b6116.chunk.js"
+  },
+  {
+    "revision": "625bdeeb2b16d0a0b5d9",
+    "url": "/portfolio/static/js/main.58dd3477.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "041825d32c0225f70deee8153e88c85f",
+    "url": "/portfolio/static/media/anime1.041825d3.png"
+  },
+  {
+    "revision": "aeec28632f4519bdb1c51013fead8ccd",
+    "url": "/portfolio/static/media/close-button.aeec2863.svg"
+  },
+  {
+    "revision": "92af5248606f6c6b447246184cec83ad",
+    "url": "/portfolio/static/media/face1.92af5248.jpg"
+  },
+  {
+    "revision": "31192cc1b189f849bc5f525f98bc547c",
+    "url": "/portfolio/static/media/young soldier.31192cc1.webp"
+  }
+]);
